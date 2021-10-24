fb50acb158454e4fc67d76309be4d37e22781011
김진현 <highcanon@gmail.com>
2021-10-24 12:03:26 +0900

    코틀린에서, xml 연결하는 binding 변수 세팅

--- app/src/main/java/com/Gaon/jin_20211024/MainActivity.kt
+++ app/src/main/java/com/Gaon/jin_20211024/MainActivity.kt
@@ -2,10 +2,17 @@ package com.Gaon.jin_20211024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.Gaon.jin_20211024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView( activity:this, R.layout.activity_main)
+
+
     }
 }
